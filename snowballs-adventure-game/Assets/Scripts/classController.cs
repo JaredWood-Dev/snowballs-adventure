04a0f1d4a3bf20d2dbe01fc92a6db4765d9aa079
Kaden Misenheimer <therealspydude@gmail.com>
2023-02-15 17:52:51 -0600

    Started work on the Class Controller, and initial work on the weapon system

--- snowballs-adventure-game/Assets/Scripts/classController.cs
+++ snowballs-adventure-game/Assets/Scripts/classController.cs
@@ -1,12 +1,31 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class classController : MonoBehaviour
 {
+    public Class[] classList = new Class[13];
+    
+    List<Class> possibleClasses = new List<Class>();
+
+    public Class currentClass;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //sets to the current class to a random class (not the last one (cleric))
+        //currentClass = classList[Random.Range(0, 12)];    
+        currentClass = classList[1];
+
+        foreach (Class i in classList)
+        {
+            possibleClasses.Add(i);
+        }
+
+        Debug.Log(currentClass.className);
+        Debug.Log(currentClass.primary.abilityName);
+        Debug.Log(currentClass.secondary.abilityName);
         
     }
 
